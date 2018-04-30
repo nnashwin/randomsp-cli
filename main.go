3deb4c0f6c9e02ea86cc62b1160667fceb044a2f
Tyler Boright <lupin3.ken@gmail.com>
2018-04-29 23:01:58 -0700

    adds a function which returns the stock pick if there are no errors and an error message when there are

--- main.go
+++ main.go
@@ -2,10 +2,52 @@ package main
 
 import (
 	"fmt"
-	"github.com/ru-lai/randomsp"
+	"github.com/fatih/color"
+	rsp "github.com/ru-lai/randomsp"
+	"github.com/urfave/cli"
+	"os"
 )
 
 func main() {
-	stock := randomsp.GetRandomSPStock()
-	fmt.Println(stock)
+	errCol := color.New(color.FgRed).SprintFunc()
+
+	app := cli.NewApp()
+	app.Name = "randomsp-cli"
+	app.Version = "0.1.0"
+	app.Usage = "a cli tool to randomly query stocks from a few major indices"
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Tyler Boright",
+			Email: "ru.lai.development@gmail.com",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		color.Magenta("Get a random stock pick!")
+		return nil
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "randomStock",
+			Aliases: []string{"rs"},
+			Usage:   "Return a random stock from any of the supported indices",
+			Action: func(c *cli.Context) error {
+				stock, err := rsp.GetRandomIndexStock()
+				if err != nil {
+					return fmt.Errorf(errCol("The randomsp failed with following error: %s. \nCheck your internet connection and try again."), err)
+				}
+
+				color.Magenta(stock)
+
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
